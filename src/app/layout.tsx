41c6d182b96c8db10b53db754e7e4d03dd4546d2
DmitryKhiliuk <khiliukbrest@gmail.com>
2023-11-06 20:35:57 +0300

    dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {Inter} from 'next/font/google'
 import './globals.css'
 import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer/footer";
+import {ThemeProvider} from "@/context/ThemeContext";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -18,13 +19,15 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-        <body className={inter.className}>
-            <div className={'container'}>
-                <Navbar/>
-                {children}
-                <Footer/>
-            </div>
-        </body>
+            <body className={inter.className}>
+                <ThemeProvider>
+                    <div className={'container'}>
+                        <Navbar/>
+                        {children}
+                        <Footer/>
+                    </div>
+                </ThemeProvider>
+            </body>
         </html>
     )
 }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 
 import styles from './page.module.css'
+import DarkModeToggle from '../DarkModeToggle/DarkModeToggle'
 
 
 
@@ -42,6 +43,7 @@ const Navbar = () => {
         <nav className={styles.container}>
             <Link href={'/'} className={styles.logo}>MyAPP</Link>
             <div className={styles.links}>
+                <DarkModeToggle/>
                 {links.map((link) => (
                     <Link key={link.id} href={link.url}>{link.title}</Link>
                 ))}
